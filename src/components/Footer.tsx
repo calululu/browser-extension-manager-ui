591341b2ffd6d419556e8e2afb2e3c823f7aaa3d
calululu <ca9delta@gmail.com>
2025-06-13 09:39:16 +0200

    init commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <div className="text-center">
+      Challenge by
+      <a
+        className="text-blue-footer"
+        href="https://www.frontendmentor.io/challenges/browser-extension-manager-ui-yNZnOfsMAp"
+      >
+        {" "}
+        Frontend Mentor.
+      </a>{" "}
+      Coded by{" "}
+      <a className="text-blue-footer" href="#">
+        Calulu
+      </a>
+      .
+    </div>
+  );
+}
