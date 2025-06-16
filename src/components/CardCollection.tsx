694ebc83d9ae24886c84a607208d83aaaf6fd996
calululu <ca9delta@gmail.com>
2025-06-16 13:49:02 +0200

    animations added

--- src/components/CardCollection.tsx
+++ src/components/CardCollection.tsx
@@ -2,8 +2,29 @@ import { type CardType } from "../types/type";
 import Card from "./Card";
 import { useModal } from "../contexts/modalContext/useModal";
 import { useExtensionsFilter } from "../contexts/extensionsFilterContext/useExtensionsFilter";
+import { motion, AnimatePresence } from "framer-motion";
 
-// CardCollection.tsx
+// Add this variant configuration
+const container = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1, // This will stagger the children's animation
+      delayChildren: 0.1, // Optional: initial delay before animation starts
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5 },
+  },
+};
+
 export default function CardCollection() {
   const { setCards, filteredCards, toggleCardActive } = useExtensionsFilter();
   const { openDeleteModal } = useModal();
@@ -13,22 +34,30 @@ export default function CardCollection() {
   };
 
   return (
-    <div className="grid grid-cols-1 items-center justify-center gap-4 xl:grid-cols-3">
-      {filteredCards.map((card: CardType, index: number) => {
-        return (
-          <Card
-            key={index}
-            name={card.name}
-            description={card.description}
-            logo={card.logo}
-            isActive={card.isActive}
-            onRemove={() =>
-              openDeleteModal(card.name, () => handleRemoveCard(card.name))
-            }
-            onToggleActive={() => toggleCardActive(card.name)}
-          />
-        );
-      })}
-    </div>
+    <motion.div
+      className="grid grid-cols-1 items-center justify-center gap-4 xl:grid-cols-3"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      <AnimatePresence>
+        {filteredCards.map((card: CardType, index: number) => {
+          return (
+            <motion.div key={index} variants={item}>
+              <Card
+                name={card.name}
+                description={card.description}
+                logo={card.logo}
+                isActive={card.isActive}
+                onRemove={() =>
+                  openDeleteModal(card.name, () => handleRemoveCard(card.name))
+                }
+                onToggleActive={() => toggleCardActive(card.name)}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
+    </motion.div>
   );
 }
